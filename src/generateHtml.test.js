caff64e1ea475ab2e3663efcd39bf00143417ee6
barkstone <barkstone2@gmail.com>
2024-07-25 10:45:38 +0900

    feat: 마크다운 파싱 및 변환 유틸 추가

---
+++ src/generateHtml.test.js
@@ -0,0 +1,45 @@
+import {beforeAll, vi, describe, it, expect, afterAll} from "vitest";
+import generateHtmlFiles from "./generateHtml.js";
+import fs from "fs";
+
+const htmlPath = 'public/html/md.html'
+const mdFilePath = 'md.md'
+let markdown;
+let expectedHtml;
+vi.mock('fs')
+beforeAll(() => {
+  fs.readFileSync.mockImplementation((filePath) => {
+    if (filePath === 'md.md') {
+      return markdown;
+    } else {
+      return JSON.stringify([`${mdFilePath}`])
+    }
+  });
+  fs.existsSync.mockImplementation(() => {
+  
+  });
+  fs.writeFileSync.mockImplementation(() => {
+  });
+})
+
+afterAll(() => {
+  vi.clearAllMocks();
+});
+
+describe("html 생성 요청 시", () => {
+  it("마크다운이 HTML로 변환된다", async () => {
+    // given
+    markdown = '# H1';
+    expectedHtml = '<h1>H1</h1>';
+
+    // when
+    await generateHtmlFiles()
+
+    // then
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      htmlPath,
+      expectedHtml,
+      'utf-8'
+    );
+  });
+});
