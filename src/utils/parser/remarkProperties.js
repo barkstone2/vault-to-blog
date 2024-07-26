488ce0c9c049558880e9893ec2dd7e9e7a23a237
barkstone <barkstone2@gmail.com>
2024-07-27 00:45:43 +0900

    feat: 프로퍼티 파싱 플러그인 추가

---
+++ src/utils/parser/remarkProperties.js
@@ -0,0 +1,57 @@
+import {visit} from "unist-util-visit";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkParseFrontmatter from 'remark-parse-frontmatter';
+
+const remarkProperties = () => {
+  return (tree) => {
+    remarkFrontmatter()(tree)
+    remarkParseFrontmatter()(tree)
+    visit(tree, 'yaml', (node, index, parent) => {
+      const newNodes = [];
+      const properties = node.value.split("\n");
+      newNodes.push({
+        type: 'html',
+        value: '<div class="property-container">'
+      });
+      
+      for (const property of properties) {
+        const pInfo = property.split(": ")
+        const title = pInfo[0];
+        const value = pInfo[1];
+        newNodes.push({
+          type: 'html',
+          value: `<div class="property-title">`
+        })
+        newNodes.push({
+          type: 'text',
+          value: `${title}`
+        })
+        newNodes.push({
+          type: 'html',
+          value: `</div>`
+        })
+        newNodes.push({
+          type: 'html',
+          value: `<div class="property-value">`
+        })
+        newNodes.push({
+          type: 'text',
+          value: `${value}`
+        })
+        newNodes.push({
+          type: 'html',
+          value: `</div>`
+        })
+      }
+      
+      newNodes.push({
+        type: 'html',
+        value: `</div>`
+      })
+      
+      parent.children.splice(index, 1, ...newNodes);
+    })
+  }
+}
+
+export default remarkProperties
