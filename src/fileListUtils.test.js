5fc6b6ff077929c47809fabfe44c7741bdd3966a
barkstone <barkstone2@gmail.com>
2024-07-27 18:50:48 +0900

    feat: 마크다운 파일 소스 디렉토리 변경

--- src/fileListUtils.test.js
+++ src/fileListUtils.test.js
@@ -2,7 +2,7 @@ import {describe, expect, it, vi, beforeAll, afterAll} from 'vitest';
 import createFileMapToJson from "./fileListUtils.js";
 import fs from 'fs';
 
-const markdownPath = 'public/markdown';
+const sourceDir = 'public/sources';
 const jsonFilePath = 'public/markdown-files.json'
 const options = { encoding: 'utf-8' };
 const jsonOf = (expectedFileList) => {return JSON.stringify(expectedFileList, null, 2)};
@@ -32,7 +32,7 @@ describe('파일 목록 JSON 생성 함수 호출 시', () => {
   it('md 확장자를 가진 파일만 목록에 포함된다.', () => {
     // given
     mockFiles = {
-      [markdownPath] : ['file1.md', 'file2.txt', 'file3.png', 'file4.mdmd', 'file5.md6'],
+      [sourceDir] : ['file1.md', 'file2.txt', 'file3.png', 'file4.mdmd', 'file5.md6'],
     };
     expectedFileMap = {
       'file1' : ['file1.md']
@@ -52,10 +52,10 @@ describe('파일 목록 JSON 생성 함수 호출 시', () => {
   it('재귀적으로 파일을 탐색해 목록에 포함한다.', () => {
     // given
     mockFiles = {
-      [markdownPath]: ['file1.md', 'dir1'],
-      [markdownPath + '/dir1']: ['file2.md', 'dir2'],
-      [markdownPath + '/dir1/dir2']: ['file3.md', 'dir3'],
-      [markdownPath + '/dir1/dir2/dir3']: ['file4.md'],
+      [sourceDir]: ['file1.md', 'dir1'],
+      [sourceDir + '/dir1']: ['file2.md', 'dir2'],
+      [sourceDir + '/dir1/dir2']: ['file3.md', 'dir3'],
+      [sourceDir + '/dir1/dir2/dir3']: ['file4.md'],
     };
     expectedFileMap = {
       'file1' : ['file1.md'],
@@ -81,8 +81,8 @@ describe('파일 목록 JSON 생성 함수 호출 시', () => {
   it('파일명이 중복되면 엔트리의 리스트에 삽입된다.', () => {
     // given
     mockFiles = {
-      [markdownPath]: ['file1.md', 'dir1'],
-      [markdownPath + '/dir1']: ['file1.md'],
+      [sourceDir]: ['file1.md', 'dir1'],
+      [sourceDir + '/dir1']: ['file1.md'],
     };
     expectedFileMap = {
       'file1' : ['file1.md', 'dir1/file1.md'],
