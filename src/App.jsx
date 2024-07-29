70f4e1ad3bd4b07213be868d926c0efed67e1d5a
barkstone <barkstone2@gmail.com>
2024-07-29 12:58:19 +0900

    feat: 사이드바 컴포넌트 추가
    - 사이드바 트리 관련 CSS 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
 import './App.css'
 import {initImageFileMap, initMarkdownFileMap} from "./utils/file/fileUtils.js";
-import {useState} from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-await initMarkdownFileMap()
-await initImageFileMap()
+import Sidebar from "./components/Sidebar.jsx";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isInitialized, setIsInitialized] = useState(false);
+  useEffect(() => {
+    async function initialize() {
+      await initMarkdownFileMap();
+      await initImageFileMap();
+      setIsInitialized(true);
+    }
+    initialize();
+  }, []);
+  
+  if (!isInitialized) {
+    return <div className="loading-layer">로딩중...</div>;
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import '../styles/sidebar.css';
+import TreeContainer from "./TreeContainer.jsx";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <TreeContainer/>
+    </div>
+  );
+}
+
+export default Sidebar;
