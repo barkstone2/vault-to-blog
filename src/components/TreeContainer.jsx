700ec30765f285c4821ba017df72387f0d39fb0b
barkstone <barkstone2@gmail.com>
2024-07-29 12:46:06 +0900

    feat: 트리 컨테이너 컴포넌트 추가

---
+++ src/components/TreeContainer.jsx
@@ -0,0 +1,19 @@
+import {useRef} from 'react';
+import {initTree, renderTree} from "../utils/treeUtils.jsx";
+
+const directoryFirstFn = ([key1, value1], [key2, value2]) => {
+  if (value1.isFile !== value2.isFile) {
+    return value1.isFile ? 1 : -1;
+  }
+  return key1.localeCompare(key2);
+};
+
+const TreeContainer = () => {
+  const tree = useRef(initTree());
+  return (
+    <div className="tree-container">
+      {renderTree(tree.current.children, '', directoryFirstFn)}
+    </div>)
+};
+
+export default TreeContainer;
