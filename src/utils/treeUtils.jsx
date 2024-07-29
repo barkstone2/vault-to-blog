06b171cdfaa19b31aeb4961393737e2bf5a4f67d
barkstone <barkstone2@gmail.com>
2024-07-29 11:46:40 +0900

    feat: 트리 유틸 추가
    - 트리 초기화, 렌더링을 담당하는 유틸 추가

---
+++ src/utils/treeUtils.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {getMarkdownFileSet} from "./file/fileUtils.js";
+import TreeItem from "../components/TreeItem.jsx";
+
+const calculateCounts = (node) => {
+  let total = 0;
+  Object.values(node).forEach((child) => {
+    if (child.children && !child.isFile) {
+      child.count += calculateCounts(child.children);
+    }
+    total += child.count;
+  });
+  return total;
+};
+
+const buildTree = (paths) => {
+  const root = {};
+  paths.forEach((path) => {
+    const parts = path.split('/');
+    let current = root;
+    parts.forEach((part, index) => {
+      if (!current[part]) {
+        current[part] = {
+          children: {},
+          count: 0,
+          isFile: index === parts.length - 1,
+        };
+      }
+      if (index === parts.length - 1) {
+        current[part].count += 1;
+      }
+      current = current[part].children;
+    });
+  });
+  
+  const tree = {};
+  tree.children = root;
+  tree.count = calculateCounts(root);
+  return tree;
+};
+
+export const initTree =  () => {
+  const fileSet = getMarkdownFileSet();
+  return buildTree(fileSet)
+}
+
+export const renderTree = (nodes, basePath = '', compareFn = () => {}) => {
+  const sortedNodes = Object.entries(nodes).sort(compareFn)
+  return sortedNodes.map(([key, value]) => {
+    const path = `${basePath}/${key}`;
+    return (
+      <React.Fragment key={path}>
+        {!value.isFile ? (
+          <TreeItem title={key + " (" + value.count + ")"} isDirectory={true}>
+            <div className="nav-children">
+              {renderTree(value.children, path, compareFn)}
+            </div>
+          </TreeItem>
+        ) : (
+          <TreeItem onClick={()=>{}} title={key}/>
+        )}
+      </React.Fragment>
+    );
+  })
+};
