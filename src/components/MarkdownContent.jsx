c3bfd289e9b75ff069f9377ee31b6861115135b7
barkstone <barkstone2@gmail.com>
2024-07-31 14:30:18 +0900

    feat: HTML 렌더링 시 백링크 훅 적용

--- src/components/MarkdownContent.jsx
+++ src/components/MarkdownContent.jsx
@@ -1,17 +1,19 @@
 import {useParams} from "react-router-dom";
+import useBacklinkNavigation from "../utils/hooks/useBacklinkNavigation.js";
 import {useEffect, useState} from "react";
 
 function MarkdownContent() {
   const { '*': filePath } = useParams();
   const [content, setContent] = useState('');
   useEffect(() => {
-    const fetchHtml = async  () => {
+    const fetchHtml = async () => {
       const response = await fetch(`/html/${filePath.replace('.md', '.html')}`);
       const text = await response.text();
       setContent(text)
     }
     fetchHtml()
   }, [filePath]);
+  useBacklinkNavigation(content);
   
   return (
     <div className="markdown-content" dangerouslySetInnerHTML={{ __html: content }}/>
