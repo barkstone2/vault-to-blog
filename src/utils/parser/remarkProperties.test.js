8102d74491b382acac89b89cf7db7745e27895c8
barkstone <barkstone2@gmail.com>
2024-07-31 16:32:41 +0900

    design: 프로퍼티 영역 디자인 추가

--- src/utils/parser/remarkProperties.test.js
+++ src/utils/parser/remarkProperties.test.js
@@ -12,10 +12,10 @@ describe('프로퍼티 파싱 요청 시', () => {
     ]);
     expectedAst = u('root', [
       u('html', '<div class="property-container">'),
-      u('html', '<div class="property-title">'),
+      u('html', '<div class="property-title" title="a">'),
       u('text', 'a'),
       u('html', '</div>'),
-      u('html', '<div class="property-value">'),
+      u('html', '<div class="property-value" title="A">'),
       u('text', 'A'),
       u('html', '</div>'),
       u('html', '</div>'),
@@ -32,16 +32,16 @@ describe('프로퍼티 파싱 요청 시', () => {
     ]);
     expectedAst = u('root', [
       u('html', '<div class="property-container">'),
-      u('html', '<div class="property-title">'),
+      u('html', '<div class="property-title" title="a">'),
       u('text', 'a'),
       u('html', '</div>'),
-      u('html', '<div class="property-value">'),
+      u('html', '<div class="property-value" title="A">'),
       u('text', 'A'),
       u('html', '</div>'),
-      u('html', '<div class="property-title">'),
+      u('html', '<div class="property-title" title="b">'),
       u('text', 'b'),
       u('html', '</div>'),
-      u('html', '<div class="property-value">'),
+      u('html', '<div class="property-value" title="B">'),
       u('text', 'B'),
       u('html', '</div>'),
       u('html', '</div>'),
