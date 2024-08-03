38e15f71583265fb09ee602bc7ee5faae0e42f9a
barkstone <barkstone2@gmail.com>
2024-08-04 00:29:18 +0900

    feat: 각 HTML 파일명을 타이틀로 렌더링

--- react-app/src/components/MarkdownContent.jsx
+++ react-app/src/components/MarkdownContent.jsx
@@ -2,24 +2,33 @@ import {useParams} from "react-router-dom";
 import useBacklinkNavigation from "../utils/hooks/useBacklinkNavigation.js";
 import {useEffect, useState} from "react";
 import '../styles/content.css'
+import {Helmet} from "react-helmet-async";
 
 function MarkdownContent() {
   const { '*': filePath } = useParams();
-  const [content, setContent] = useState('');
+  const [innerHtml, setInnerHtml] = useState({});
   useEffect(() => {
     const fetchHtml = async () => {
       const path = filePath ? `/html/${filePath.replace('.md', '.html')}` : `/default/home.html`;
       const response = await fetch(`${path}`);
-      const text = await response.text();
-      setContent(`<div class="content-title">${filePath.split('/').pop()}</div>` + text);
-    }
+      const content = await response.text();
+      const title = filePath.split('/').pop().replace('.md', '');
+      const newInnerHtml = {
+        title: title,
+        content: `<div class="content-title">${title}</div>` + content
+      };
+      setInnerHtml(newInnerHtml)
+    };
     fetchHtml()
   }, [filePath]);
-  useBacklinkNavigation(content);
+  useBacklinkNavigation(innerHtml.content);
   
   return (
     <div className="content-container">
-      <div className="markdown-content" dangerouslySetInnerHTML={{ __html: content }}/>
+      <Helmet>
+        <title>{innerHtml.title}</title>
+      </Helmet>
+      <div className="markdown-content" dangerouslySetInnerHTML={{__html: innerHtml.content}}/>
     </div>
   );
 }
