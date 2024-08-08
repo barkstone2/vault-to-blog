d8290b27d78c3af1f4f310ba5176a6c2f7453beb
barkstone <barkstone2@gmail.com>
2024-08-08 14:43:18 +0900

    feat: 백링크 파싱 시 옵시디언 클래스 추가
    - 매핑에 실패하는 경우에도 빈 링크 추가

--- react-app/src/utils/parser/remarkBacklink.js
+++ react-app/src/utils/parser/remarkBacklink.js
@@ -2,54 +2,57 @@ import { visit } from 'unist-util-visit';
 import {getMarkdownFileMap} from "../file/fileUtils.js";
 
 const remarkBacklink = () => {
-  const fileMap = getMarkdownFileMap()
-  return (tree) => {
-    visit(tree, ['text'], (node, index, parent) => {
-      const linkRegex = /\[\[([^\]]+)]]/g;
-      const matches = [...node.value.matchAll(linkRegex)];
-      if (matches.length > 0) {
-        const newNodes = [];
-        let lastIndex = 0;
-        
-        matches.forEach((match) => {
-          const [fullMatch, linkText] = match;
-          const linkParts = linkText.split('|');
-          const targetName = linkParts[0];
-          const display = linkParts[1] ? linkParts[1] : linkParts[0];
-          let targetPath;
-          if (fileMap[targetName]) {
-            targetPath = fileMap[targetName][0];
-          }
-          
-          if (targetPath) {
-            if (match.index > lastIndex) {
-              newNodes.push({
-                type: 'text',
-                value: node.value.slice(lastIndex, match.index),
-              });
-            }
-            
-            const url = `/${targetPath}`;
-            newNodes.push({
-              type: 'html',
-              value: `<a href="${url}" class="backlink">${display}</a>`,
-            });
-            
-            lastIndex = match.index + fullMatch.length;
-          }
-        });
-        
-        if (lastIndex < node.value.length) {
-          newNodes.push({
-            type: 'text',
-            value: node.value.slice(lastIndex),
-          });
-        }
-        
-        parent.children.splice(index, 1, ...newNodes);
-      }
-    });
-  }
-}
+	const fileMap = getMarkdownFileMap()
+	return (tree) => {
+		visit(tree, ['text'], (node, index, parent) => {
+			const linkRegex = /(?<!!)\[\[([^\]]+)]]/g;
+			const matches = [...node.value.matchAll(linkRegex)];
+			if (matches.length > 0) {
+				const newNodes = [];
+				let lastIndex = 0;
+				
+				matches.forEach((match) => {
+					const [fullMatch, linkText] = match;
+					const linkParts = linkText.split('|');
+					const targetName = linkParts[0];
+					const display = linkParts[1] ? linkParts[1] : linkParts[0];
+					let targetPath;
+					if (fileMap[targetName]) {
+						targetPath = fileMap[targetName][0];
+					}
+					if (match.index > lastIndex) {
+						newNodes.push({
+							type: 'text',
+							value: node.value.slice(lastIndex, match.index),
+						});
+					}
+					
+					if (targetPath) {
+						const url = `/${targetPath}`;
+						newNodes.push({
+							type: 'html',
+							value: `<a href="${url}" class="internal-link" data-href="${url}" target="_blank" rel="noopener">${display}</a>`,
+						});
+					} else {
+						newNodes.push({
+							type: 'html',
+							value: `<a href="#" class="internal-link is-unresolved" data-href="${targetName}" target="_blank" rel="noopener" tabindex="-1">${display}</a>`
+						});
+					}
+					lastIndex = match.index + fullMatch.length;
+				});
+				
+				if (lastIndex < node.value.length) {
+					newNodes.push({
+						type: 'text',
+						value: node.value.slice(lastIndex),
+					});
+				}
+				
+				parent.children.splice(index, 1, ...newNodes);
+			}
+		});
+	}
+};
 
 export default remarkBacklink;
--- react-app/src/utils/parser/remarkBacklink.test.js
+++ react-app/src/utils/parser/remarkBacklink.test.js
@@ -20,7 +20,7 @@ let inputAst;
 let expectedAst;
 
 describe("remarkBacklink 플러그인 동작 시", () => {
-  it("[[]]으로 감싸진 텍스트를 html 노드로 변환한다.", () => {
+  it("[[]]으로 감싸진 텍스트에 대한 매핑 정보가 있으면 internal 링크 노드로 변환한다.", () => {
     inputAst = u('root', [
       u('paragraph', [
         u('text', '[[md]]')
@@ -28,22 +28,54 @@ describe("remarkBacklink 플러그인 동작 시", () => {
     ]);
     expectedAst = u('root', [
       u('paragraph', [
-        u('html', '<a href="/md.md" class="backlink">md</a>')
+        u('html', '<a href="/md.md" class="internal-link" data-href="/md.md" target="_blank" rel="noopener">md</a>')
       ])
     ]);
     remarkBacklink()(inputAst)
     expect(inputAst).toEqual(expectedAst)
   });
   
-  it("[[]]으로 감싸진 텍스트가 fileMap에 없으면 html 노드로 변환하지 않는다", () => {
+  it("[[]]으로 감싸진 텍스트에 대한 매핑 정보가 없으면 unresolved 링크 노드로 변환한다.", () => {
     inputAst = u('root', [
       u('paragraph', [
-        u('text', '[[md.png]]')
+        u('text', '[[unresolved]]')
       ])
     ]);
     expectedAst = u('root', [
       u('paragraph', [
-        u('text', '[[md.png]]')
+        u('html', `<a href="#" class="internal-link is-unresolved" data-href="unresolved" target="_blank" rel="noopener" tabindex="-1">unresolved</a>`)
+      ])
+    ]);
+    remarkBacklink()(inputAst)
+    expect(inputAst).toEqual(expectedAst)
+  });
+  
+  it("![[]]으로 감싸진 텍스트는 링크로 변환하지 않는다.", () => {
+    inputAst = u('root', [
+      u('paragraph', [
+        u('text', '![[image]]')
+      ])
+    ]);
+    expectedAst = u('root', [
+      u('paragraph', [
+        u('text', '![[image]]')
+      ])
+    ]);
+    remarkBacklink()(inputAst)
+    expect(inputAst).toEqual(expectedAst)
+  });
+  
+  it("[[]]으로 감싸진 텍스트 앞뒤에 있는 텍스트가 유지된다.", () => {
+    inputAst = u('root', [
+      u('paragraph', [
+        u('text', 'before[[md]]after')
+      ])
+    ]);
+    expectedAst = u('root', [
+      u('paragraph', [
+        u('text', 'before'),
+        u('html', '<a href="/md.md" class="internal-link" data-href="/md.md" target="_blank" rel="noopener">md</a>'),
+        u('text', 'after')
       ])
     ]);
     remarkBacklink()(inputAst)
@@ -58,7 +90,22 @@ describe("remarkBacklink 플러그인 동작 시", () => {
     ]);
     expectedAst = u('root', [
       u('paragraph', [
-        u('html', '<a href="/md.md" class="backlink">display</a>')
+        u('html', '<a href="/md.md" class="internal-link" data-href="/md.md" target="_blank" rel="noopener">display</a>'),
+      ])
+    ]);
+    remarkBacklink()(inputAst)
+    expect(inputAst).toEqual(expectedAst)
+  });
+  
+  it("[[]]으로 감싸진 텍스트에 매핑 정보가 없어도 표시용 이름이 있으면 해당 이름으로 표시한다.", () => {
+    inputAst = u('root', [
+      u('paragraph', [
+        u('text', '[[unresolved|display]]')
+      ])
+    ]);
+    expectedAst = u('root', [
+      u('paragraph', [
+        u('html', '<a href="#" class="internal-link is-unresolved" data-href="unresolved" target="_blank" rel="noopener" tabindex="-1">display</a>'),
       ])
     ]);
     remarkBacklink()(inputAst)
