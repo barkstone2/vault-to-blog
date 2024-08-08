21af38b7c137a7530a298566b6c0fab7ef5c45eb
barkstone <barkstone2@gmail.com>
2024-08-08 14:03:53 +0900

    design: 앱에 옵시디언 클래스 추가
    - App.jsx에 옵시디언 클래스 추가
    - index.html에 옵시디언 클래스 추가
    - main.jsx에 옵시디언 스타일시트 임포트
    - 기존 스타일 제거

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './styles/obsidian.css'
 import {BrowserRouter} from "react-router-dom";
 import {HelmetProvider} from "react-helmet-async";
 
