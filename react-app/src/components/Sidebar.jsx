53995fa32dfaa1e57962dac33ad9b926afbb35b5
barkstone <barkstone2@gmail.com>
2024-08-08 13:57:15 +0900

    feat: 사이드바에 옵시디언 클래스 추가

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -1,11 +1,22 @@
 import '../styles/sidebar.css';
 import TreeContainer from "./TreeContainer.jsx";
+import TabHeaderContainer from "./sidebar/TabHeaderContainer.jsx";
+import SidebarEmptyState from "./sidebar/SidebarEmptyState.jsx";
+
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <TreeContainer/>
+    <div className="workspace-split mod-horizontal mod-sidedock mod-left-split" style={{width: '298.503px'}}>
+      <hr className="workspace-leaf-resize-handle" style={{opacity: 1}}/>
+      <SidebarEmptyState/>
+      <div className="workspace-tabs mod-top mod-top-left-space">
+        <hr className="workspace-leaf-resize-handle"/>
+        <TabHeaderContainer></TabHeaderContainer>
+        <div className="workspace-tab-container">
+          <TreeContainer datatype="file-explorer"/>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
