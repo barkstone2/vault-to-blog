d042371e5e1debd73ec1ae7bb7c72d516ec17209
barkstone <barkstone2@gmail.com>
2024-08-08 14:09:35 +0900

    design: 마크다운 컨텐츠 컴포넌트에 옵시디언 클래스 추가

--- react-app/src/components/MarkdownContent.jsx
+++ react-app/src/components/MarkdownContent.jsx
@@ -5,7 +5,7 @@ import '../styles/content.css'
 import {Helmet} from "react-helmet-async";
 
 function MarkdownContent() {
-  const { '*': filePath } = useParams();
+  const {'*': filePath} = useParams();
   const [innerHtml, setInnerHtml] = useState({});
   useEffect(() => {
     const fetchHtml = async () => {
@@ -15,7 +15,7 @@ function MarkdownContent() {
       const title = filePath.split('/').pop().replace('.md', '');
       const newInnerHtml = {
         title: title,
-        content: `<div class="content-title">${title}</div>` + content
+        content: content
       };
       setInnerHtml(newInnerHtml)
     };
@@ -24,11 +24,43 @@ function MarkdownContent() {
   useBacklinkNavigation(innerHtml.content);
   
   return (
-    <div className="content-container">
+    <div className="workspace-split mod-vertical mod-root">
       <Helmet>
         <title>{innerHtml.title}</title>
       </Helmet>
-      <div className="markdown-content" dangerouslySetInnerHTML={{__html: innerHtml.content}}/>
+      <hr className="workspace-leaf-resize-handle"/>
+      <div className="workspace-tabs mod-top">
+        <div className="workspace-tab-header-container"></div>
+        <div className="workspace-tab-container">
+          <div className="workspace-leaf">
+            <hr className="workspace-leaf-resize-handle"/>
+            <div className="workspace-leaf-content" datatype="markdown" datamode="preview">
+              <div className="view-header">
+              </div>
+              <div className="view-content">
+                <div className="markdown-reading-view" style={{width: '100%', height: '100%'}}>
+                  <div
+                    className="markdown-preview-view markdown-rendered node-insert-event is-readable-line-width allow-fold-headings show-indentation-guide allow-fold-lists show-properties"
+                    tabIndex="-1" style={{tabSize: 4}}>
+                    <div className="markdown-preview-sizer markdown-preview-section"
+                         style={{paddingBottom: '336px', minHeight: '1054px'}}>
+                      <div className="markdown-preview-pusher"
+                           style={{width: '1px', height: '0.1px', marginBottom: '0px'}}></div>
+                      <div className="mod-header">
+                        <div className="inline-title">{innerHtml.title}</div>
+                        <div className="metadata-container" tabIndex="-1" data-property-count="3">
+                          <div className="metadata-properties-heading" tabIndex="0"></div>
+                        </div>
+                      </div>
+                      <div dangerouslySetInnerHTML={{__html: innerHtml.content}}/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
