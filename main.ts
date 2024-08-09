8030c6f5b36deddfc8e9c5a31a1af2fcc0fe1fa3
barkstone <barkstone2@gmail.com>
2024-08-09 10:59:16 +0900

    feat: 옵시디언 설정 인터페이스 추가
    - 설정 로드, 세이브 메서드 추가

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 import {Plugin} from 'obsidian';
 
+export interface ObsidianToBlogSettings {
+ sourceDir: string;
+ repositoryUrl: string;
+ isActivated: boolean;
+}
+
+const DEFAULT_SETTINGS: ObsidianToBlogSettings = {
+ sourceDir: '',
+ repositoryUrl: '',
+ isActivated: false,
+}
+
 export default class ObsidianToBlog extends Plugin {
+	settings: ObsidianToBlogSettings;
 	async onload() {
+		await this.loadSettings();
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
