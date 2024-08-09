354840d5e9e92e1546f25e81725440be166f3101
barkstone <barkstone2@gmail.com>
2024-08-09 11:12:11 +0900

    feat: 옵시디언 플러그인 상태바 항목 추가
    - 상태바 항목 스타일 추가
    - 상태바 항목 클릭 시 동작 뼈대 정의

---
+++ src/layout/statusBar.ts
@@ -0,0 +1,26 @@
+import ObsidianToBlog from "../../main";
+import {Notice} from "obsidian";
+
+export class StatusBar {
+	private iconSrc = 'https://raw.githubusercontent.com/barkstone2/obsidian-to-blog/main/react-app/public/favicon.ico'
+	constructor(private statusBarEl: HTMLElement, private readonly plugin: ObsidianToBlog) {
+		statusBarEl.createEl('img', {cls: 'status-bar-icon', attr: {src: this.iconSrc}})
+		statusBarEl.addClass('mod-clickable')
+		statusBarEl.ariaLabel = 'Publish to GitHub Page.';
+		statusBarEl.setAttribute("data-tooltip-position", "top");
+		statusBarEl.onClickEvent(async () => {
+			if (this.plugin.settings.isActivated) {
+			} else {
+				new Notice("Activate before publishing.")
+			}
+		})
+	}
+
+	activate() {
+		this.statusBarEl.removeClass('grayscale')
+	}
+
+	inactivate() {
+		this.statusBarEl.addClass('grayscale')
+	}
+}
