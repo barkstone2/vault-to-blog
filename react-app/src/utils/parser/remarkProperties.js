9d8c7174df18590292c79853926c33762441a43f
barkstone <barkstone2@gmail.com>
2024-08-13 14:07:01 +0900

    refactor: 리마크 프로퍼티 플러그인 리팩터링

--- react-app/src/utils/parser/remarkProperties.js
+++ react-app/src/utils/parser/remarkProperties.js
@@ -10,53 +10,97 @@ import {addNewTag} from "../remarkUtils.js";
 const remarkProperties = (options = {}) => {
   return (tree) => {
     const newNodes = [];
-    addNewTag(newNodes, `<div class="markdown-preview-pusher" style="width: 1px; height: 0.1px; margin-bottom: 0;"></div><div class="mod-header">`, `</div>`,
+    addMarkdownPreviewPusher(newNodes);
+    addNewTag(newNodes,
+      `<div class="mod-header">`, `</div>`,
       () => {
-        newNodes.push({
-          type: 'html',
-          value: `<div class="inline-title" tabindex="-1">${options.title}</div>`
-        })
-        
-        if (tree.children[0]?.type === 'yaml') {
-          addNewTag(
-            newNodes,
-            '<div class="metadata-container" tabindex="-1">' +
-            '<div class="metadata-properties-heading" tabindex="-1">' +
-            '<div class="collapse-indicator collapse-icon">' +
-            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon right-triangle"><path d="M3 8L12 17L21 8"></path></svg>' +
-            '</div>' +
-            '<div class="metadata-properties-title">Properties</div>' +
-            '</div>' +
-            '<div class="metadata-content">' +
-            '<div class="metadata-properties">',
-            '</div>' +
-            '</div>' +
-            '</div>',
-            () => {
-              const yaml = tree.children[0];
-              const properties = yaml.value.split('\n');
-              for (let index = 0; index < properties.length; index++) {
-                const property = properties[index];
-                const {key, value} = parseKeyAndValue(property)
-                const type = getPropertyType(key);
-                addNewTag(
-                  newNodes,
-                  `<div class="metadata-property" data-property-key="${key}" data-property-type="${type}">`,
-                  '</div>',
-                  () => {
-                    addPropertyKey(newNodes, key, type);
-                    if (type === 'multitext' || type === 'tags') {
-                      index = addMultiPropertyValue(newNodes, type, properties, index);
-                    } else {
-                      addPropertyValue(newNodes, value, type);
-                    }
-                  })
-              }
-            });
+        addInlineTitle(newNodes, options.title);
+        if (hasProperties(tree)) {
+          addPropertiesContainer(newNodes, tree);
+          removePropertiesMetadata(tree);
         }
       })
-    tree.children.splice(0, 1, ...newNodes);
+    tree.children.unshift(...newNodes);
   }
+}
+
+function addMarkdownPreviewPusher(newNodes) {
+  newNodes.push({
+    type: 'html',
+    value: `<div class="markdown-preview-pusher" style="width: 1px; height: 0.1px; margin-bottom: 0;"></div>`
+  })
+}
+
+function addInlineTitle(newNodes, title) {
+  newNodes.push({
+    type: 'html',
+    value: `<div class="inline-title" tabindex="-1">${title}</div>`
+  })
+}
+
+function hasProperties(tree) {
+  return tree.children[0]?.type === 'yaml';
+}
+
+function addPropertiesContainer(newNodes, tree) {
+  addNewTag(newNodes,
+    '<div class="metadata-container" tabindex="-1">', '</div>',
+    () => {
+      addPropertiesHeading(newNodes);
+      addPropertyContent(newNodes, tree);
+    })
+}
+
+function addPropertiesHeading(newNodes) {
+  const collapseIconTag =
+    '<div class="collapse-indicator collapse-icon">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon right-triangle">' +
+    '<path d="M3 8L12 17L21 8"></path>' +
+    '</svg>' +
+    '</div>';
+  const propertiesTitleTag = '<div class="metadata-properties-title">Properties</div>';
+  const propertiesHeading = '<div class="metadata-properties-heading" tabindex="-1">' + collapseIconTag + propertiesTitleTag + '</div>'
+  newNodes.push({
+    type: 'html',
+    value: propertiesHeading
+  })
+}
+
+function addPropertyContent(newNodes, tree) {
+  addNewTag(newNodes,
+    '<div class="metadata-content">', '</div>',
+    () => {
+      addNewTag(newNodes,
+        '<div class="metadata-properties">', '</div>',
+        () => {
+          const yaml = tree.children[0];
+          const properties = yaml.value.split('\n');
+          for (let index = 0; index < properties.length; index++) {
+            const {key, value} = parseKeyAndValue(properties[index])
+            const type = getPropertyType(key);
+            index = addProperty(newNodes, key, type, index, properties, value);
+          }
+        });
+    })
+}
+
+function addProperty(newNodes, key, type, index, properties, value) {
+  addNewTag(
+    newNodes,
+    `<div class="metadata-property" data-property-key="${key}" data-property-type="${type}">`, '</div>',
+    () => {
+      addPropertyKey(newNodes, key, type);
+      if (type === 'multitext' || type === 'tags') {
+        index = addMultiPropertyValue(newNodes, type, properties, index);
+      } else {
+        addPropertyValue(newNodes, value, type);
+      }
+    })
+  return index;
+}
+
+function removePropertiesMetadata(tree) {
+  tree.children.splice(0, 1);
 }
 
 export default remarkProperties
