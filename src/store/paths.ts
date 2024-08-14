080deaab48cc57f292088ff5b24c0fb0cfd81276
barkstone <barkstone2@gmail.com>
2024-08-14 13:03:59 +0900

    feat: 버전이 사용되는 경로 동적 결정
    - URL 결정 시에도 동적 결정

--- src/store/paths.ts
+++ src/store/paths.ts
@@ -1,36 +1,34 @@
 import {App, FileSystemAdapter} from "obsidian";
+import {ObsidianToBlogSettings} from "../../main";
 
 export class Paths {
 	app: App;
 	vaultPath: string;
 	pluginPath: string;
-	reactVersionPath: string;
-	reactPath: string;
-	reactZipPath: string;
-	gitPath: string
 	gitBackupPath: string;
-	sourceDestPath: string;
-	sourcePublicPath: string;
 	typeJsonSourcePath: string;
-	typeJsonDestPath: string;
+	reactVersionPath;
+	reactPath;
+	reactZipPath;
+	gitPath
+	sourceDestPath;
+	sourcePublicPath;
+	typeJsonDestPath;
 
-	constructor(app: App, version: string) {
+	constructor(app: App, settings: ObsidianToBlogSettings) {
 		this.app = app;
 		this.vaultPath = this.getVaultPath();
 		this.pluginPath = `${this.vaultPath}/.obsidian/plugins/obsidian-to-blog`;
-
-		this.reactVersionPath = `${this.pluginPath}/react-app/${version}`
-		this.reactPath = `${this.reactVersionPath}/react-app`;
-		this.reactZipPath = `${this.reactVersionPath}/react-app.zip`
-
-		this.gitPath = `${this.reactPath}/.git`;
 		this.gitBackupPath = `${this.pluginPath}/.git-backup`
-
-		this.sourceDestPath = `${this.reactPath}/public/sources`;
-		this.sourcePublicPath = `${this.reactPath}/public`;
-
 		this.typeJsonSourcePath = `${this.vaultPath}/.obsidian/types.json`;
-		this.typeJsonDestPath = `${this.sourcePublicPath}/types.json`;
+
+		this.reactVersionPath = () => `${this.pluginPath}/react-app/${settings.version}`
+		this.reactPath = () => `${this.reactVersionPath()}/react-app`;
+		this.reactZipPath = () => `${this.reactVersionPath()}/react-app.zip`
+		this.gitPath = () => `${this.reactPath()}/.git`;
+		this.sourceDestPath = () => `${this.reactPath()}/public/sources`;
+		this.sourcePublicPath = () => `${this.reactPath()}/public`;
+		this.typeJsonDestPath = () => `${this.sourcePublicPath()}/types.json`;
 	}
 
 	private getVaultPath() {
