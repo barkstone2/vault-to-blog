b39b2985761316b5df73c4299497fe906a065982
barkstone <barkstone2@gmail.com>
2024-08-15 10:56:51 +0900

    feat: git 처리 시 simple-git 사용
    - 콜백 대신 simple-git을 사용한 async/await로 전환
    - simple-git 최신 버전 사용 시 node:events 오류 발생하므로 사용량이 가장 높은 3.16.0 버전 사용

--- src/utils/gitUtils.ts
+++ src/utils/gitUtils.ts
@@ -1,8 +1,9 @@
-import {spawn} from "child_process";
 import {Notice} from "obsidian";
 import OTBPlugin, {ObsidianToBlogSettings} from "../../main";
 import {findFiles} from "./fsUtils";
+import simpleGit from 'simple-git';
 
+const git = simpleGit();
 export class GitUtils {
 	plugin: OTBPlugin;
 	settings: ObsidianToBlogSettings;
@@ -12,140 +13,87 @@ export class GitUtils {
 	}
 
 	async initializeGit(options: { cwd: string }, noticeDuration: number) {
-		return new Promise(resolve => {
-			const child = spawn('git', ['init'], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of Git initialization.\n${error.message}`;
-				new Notice(message, noticeDuration)
-				console.log(message)
-			})
-			child.on('close', (code) => {
-				if (code === 0) {
-					resolve(true);
-					new Notice('Succeeded in initializing Git.', noticeDuration)
-				} else {
-					new Notice('Failed to initialize Git.', noticeDuration)
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.init()
+			new Notice('Succeeded in initializing Git.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to initialize Git.\n${error.message}`;
+			new Notice(message, noticeDuration)
+			console.error(message)
+		}
 	}
 
 	async addRemote(options: { cwd: string }, noticeDuration: number) {
-		return new Promise(resolve => {
-			const child = spawn('git', ['remote', 'add', 'blog', this.settings.repositoryUrl], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of adding the remote.\n ${error.message}`;
-				new Notice(message, noticeDuration);
-				console.log(message)
-			})
-			child.on('close', (code) => {
-				if (code === 0) {
-					resolve(true);
-					new Notice('Succeeded in adding remote.', noticeDuration)
-				} else {
-					new Notice("Failed to add remote. Make sure a remote named 'blog' is available.", noticeDuration)
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.addRemote('blog', this.settings.repositoryUrl);
+			new Notice('Succeeded in adding remote.', noticeDuration);
+		} catch (error) {
+			const message = `Failed to add remote. Make sure a remote named 'blog' is available.\n ${error.message}`;
+			new Notice(message, noticeDuration);
+			console.error(message)
+		}
 	}
 
 	async removeRemote(options: { cwd: string }, noticeDuration: number) {
-		return new Promise(resolve => {
-			const child = spawn('git', ['remote', 'remove', 'blog'], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of removing the remote.\n ${error.message}`;
-				new Notice(message, noticeDuration);
-				console.log(message)
-			})
-			child.on('close', (code) => {
-				if (code === 0) {
-					resolve(true);
-					new Notice('Succeeded in removing remote.', noticeDuration)
-				} else {
-					new Notice("Failed to remove remote.", noticeDuration)
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.removeRemote('blog');
+			new Notice('Succeeded in removing remote.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to remove remote.\n${error.message}`;
+			new Notice(message, noticeDuration);
+			console.error(message)
+		}
 	}
 
 	async stageAllChanges(options: { cwd: string }, noticeDuration: number) {
-		return new Promise(resolve => {
-			const child = spawn('git', ['add', '.'], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of staging.\n${error.message}`;
-				new Notice(message, noticeDuration);
-				console.log(message)
-			})
-			child.on('close', (code) => {
-				if (code === 0) {
-					resolve(true);
-					new Notice('Succeeded in staging all changes.', noticeDuration)
-				} else {
-					new Notice('Failed to stage all changes.', noticeDuration)
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.add('.');
+			new Notice('Succeeded in staging all changes.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to stage all changes.\n${error.message}`;
+			new Notice(message, noticeDuration);
+			console.error(message)
+		}
 	}
 
 	async commitChanges(options: {cwd: string}, noticeDuration: number, message = "Blog published by OTB") {
-		return new Promise((resolve, reject) => {
-			const child = spawn('git', ['commit', '-m', message], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of committing.\n ${error.message}`;
-				new Notice(message, noticeDuration)
-				console.log(message)
-				reject(error);
-			});
-			child.on('close', (code) => {
-				if (code == 0) {
-					new Notice('Succeeded in committing changes.', noticeDuration)
-					resolve(true);
-				} else {
-					new Notice('Failed to commit changes. There might be no changes to commit.', noticeDuration)
-					reject();
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.commit(message);
+			new Notice('Succeeded in committing changes.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to commit changes. There might be no changes to commit.\n${error.message}`;
+			new Notice(message, noticeDuration)
+			console.error(message)
+		}
 	}
 
 	async pushToRemote(options: { cwd: string }, noticeDuration: number) {
-		return new Promise(resolve => {
-			const child = spawn('git', ['push', 'blog', 'main', '-f'], options);
-			child.on('error', (error) => {
-				const message = `Failed to start the process of pushing.\n${error.message}`;
-				new Notice(message, noticeDuration)
-				console.log(message)
-			});
-			child.on('close', (code) => {
-				if (code == 0) {
-					resolve(true);
-					new Notice('Succeeded in pushing to remote.', noticeDuration)
-				} else {
-					new Notice('Failed to pushing to remote. There might be no changes to push.', noticeDuration)
-				}
-			})
-		});
+		try {
+			await git.cwd(options.cwd);
+			await git.push('blog', 'main', {'-f' : null});
+			new Notice('Succeeded in pushing to remote.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to pushing to remote. There might be no changes to push.\n${error.message}`;
+			new Notice(message, noticeDuration)
+			console.error(message)
+		}
 	}
 
 	async stageReactApp(options: { cwd: string }, noticeDuration: number) {
-		const files = await findFiles(options.cwd, 'public/sources');
-		return new Promise((resolve, reject) => {
-			const child = spawn('git', ['add', ...files], options);
-
-			child.on('error', (error) => {
-				const message = `Failed to start the process of staging react-app.\n${error.message}`;
-				new Notice(message, noticeDuration);
-				console.log(message)
-				reject(error)
-			})
-			child.on('close', (code) => {
-				if (code === 0) {
-					new Notice('Succeeded in staging react-app.', noticeDuration)
-				} else {
-					new Notice('Failed to stage react-app.', noticeDuration)
-				}
-				resolve(true);
-			})
-		});
+		try {
+			const files = await findFiles(options.cwd, 'public/sources');
+			await git.cwd(options.cwd);
+			await git.add(files);
+			new Notice('Succeeded in staging react-app.', noticeDuration)
+		} catch (error) {
+			const message = `Failed to stage react-app.\n${error.message}`;
+			new Notice(message, noticeDuration);
+			console.error(message)
+		}
 	}
 }
