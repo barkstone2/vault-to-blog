42d7331e0d300809886cbda0d019f8cfb96a7d2b
barkstone <barkstone2@gmail.com>
2024-08-16 15:01:43 +0900

    fix: 다운로드 주소 OTB 리포지토리를 사용하도록 변경

--- src/store/urls.ts
+++ src/store/urls.ts
@@ -1,8 +1,9 @@
 import {ObsidianToBlogSettings} from "../../main";
 
 export class Urls {
+    otbRepositoryURL = 'https://github.com/barkstone2/obsidian-to-blog'
     reactAppUrl;
     constructor(settings: ObsidianToBlogSettings) {
-        this.reactAppUrl = () => `${settings.repositoryUrl}/releases/download/${settings.version}/react-app.zip`;
+        this.reactAppUrl = () => `${this.otbRepositoryURL}/releases/download/${settings.version}/react-app.zip`;
     }
 }
