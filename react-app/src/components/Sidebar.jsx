06669b78dcf717777373e3607abd2e742e8512fa
barkstone <barkstone2@gmail.com>
2024-08-16 13:17:57 +0900

    design: 구현되지 않은 기능 관련 버튼 주석 처리

--- react-app/src/components/Sidebar.jsx
+++ react-app/src/components/Sidebar.jsx
@@ -6,7 +6,8 @@ import SidebarEmptyState from "./sidebar/SidebarEmptyState.jsx";
 const Sidebar = () => {
   return (
     <div className="workspace-split mod-horizontal mod-sidedock mod-left-split" style={{width: '298.503px'}}>
-      <hr className="workspace-leaf-resize-handle" style={{opacity: 1}}/>
+      {/* TODO 사이드바 크기 조절 기능 추가 후 주석 해제 */}
+      {/*<hr className="workspace-leaf-resize-handle" style={{opacity: 1}}/>*/}
       <SidebarEmptyState/>
       <div className="workspace-tabs mod-top mod-top-left-space">
         <hr className="workspace-leaf-resize-handle"/>
