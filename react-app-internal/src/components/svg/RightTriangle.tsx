6e7a6a9df0ceb80d510f8d71e22bfbddbf0d3623
barkstone <barkstone2@gmail.com>
2025-01-19 20:03:09 +0900

    feat: Add svg components for using svg icon

---
+++ react-app-internal/src/components/svg/RightTriangle.tsx
@@ -0,0 +1,9 @@
+export function RightTriangle() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+             className="svg-icon right-triangle">
+            <path d="M3 8L12 17L21 8"></path>
+        </svg>
+    );
+}
---
+++ react-app-internal/src/components/svg/SidebarRightToggleSvg.tsx
@@ -0,0 +1,14 @@
+export function SidebarRightToggleSvg() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+             className="svg-icon sidebar-right">
+            <path
+                d="M3 3H21C22.1046 3 23 3.89543 23 5V19C23 20.1046 22.1046 21 21 21H3C1.89543 21 1 20.1046 1 19V5C1 3.89543 1.89543 3 3 3Z"></path>
+            <path d="M14 4V20"></path>
+            <path d="M20 7H17"></path>
+            <path d="M20 10H17"></path>
+            <path d="M20 13H17"></path>
+        </svg>
+    );
+}
